26e604dda6f3bb06511e893b1c6eec516b8c2974
Ed_Mvchn <iamtarx@gmail.com>
2020-03-02 20:49:01 +0200

    my 3 files

---
+++ main.js
@@ -0,0 +1,30 @@
+//FizzBuzz
+
+//Fizz % 3
+//Buzz % 5
+//FizzBuzz % 3 % 5
+
+/*let i = prompt('Введите число до 100');
+
+for (let i = 1; i <= 100; i++); 
+    if (i % 3 == 0) {
+    alert('Fizz');
+}
+for (let i = 1; i <= 100; i++); 
+    if (i % 5 == 0) {
+    alert('Buzz');
+}
+for (let i = 1; i <= 100; i++);
+    if (i % 3 == 0)
+    if (i % 5 == 0) {
+    alert('FizzBuzz')
+}
+*/
+
+let smile = ':)';
+let smileList = '';
+
+for (let i = 1; i<= 5; i = i + 1) {
+    smileList = smileList + smile;
+}    
+console.log(smileList)
