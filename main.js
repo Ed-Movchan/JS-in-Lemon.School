8294b9d8a4000ee7c7d4d96e31d10fcf55a94931
Ed_Mvchn <iamtarx@gmail.com>
2020-03-09 23:40:36 +0200

    DZ1

--- main.js
+++ main.js
@@ -1,30 +1,98 @@
-//FizzBuzz
+// Lemon.School, JS
+// DZ1
 
-//Fizz % 3
-//Buzz % 5
-//FizzBuzz % 3 % 5
+Задача №0
 
-/*let i = prompt('Введите число до 100');
+let a = 1
+let b = 2
+let c = 3
+const d = 4
+const e = 5
+const f = 6
+var m = 7
+var n = 8
+var p = 9
 
-for (let i = 1; i <= 100; i++); 
-    if (i % 3 == 0) {
-    alert('Fizz');
+console.log(a,b,c,d,e,f,m,n,p)
+
+
+Задача №1
+
+let a = 5
+let b = 8
+
+if (a+b==13) {
+    console.log(a)
+} else {
+console.log(b)
 }
-for (let i = 1; i <= 100; i++); 
-    if (i % 5 == 0) {
-    alert('Buzz');
+
+
+Задача №2
+
+let year = prompt('Введите год', '');
+
+if (year % 4 == 0 && year % 400 !== 0) {
+    alert(366)
+} else {
+alert(365)
 }
-for (let i = 1; i <= 100; i++);
-    if (i % 3 == 0)
-    if (i % 5 == 0) {
-    alert('FizzBuzz')
-}
+
+
+Задача №3
+
+let a = prompt('Введите число1', '');
+let b = prompt('Введите число2', '');
+let c = prompt('Введите число3', '');
+
+if ((a<b && a>c) || (a>b && a<c))
+alert(a);
+
+if ((b<c && b>a) || (b>c && b<a))
+alert(b);
+
+if ((c<a && c>b) || (c>a && c<b))
+alert(c);
+
+
+Задача №4
+
+let a = prompt('Введите число1', '');
+let b = prompt('Введите число2', '');
+let c = prompt('Введите число3', '');
+
+if (a>b && a>c)
+alert(a);
+
+if (b>c && b>a)
+alert(b);
+
+if (c>a && c>b)
+alert(c);
+
+
+Задача №5
+
+let a = 16 // а - условные единицы в кармане
+
+if (a-25 <0)
+alert('Мы в минусе!')
+
+if (a-14 >0)
+alert('Мы в плюсе!')
+/* 
+<0 - мы должны кому-то
+>0 - у нас еще осталось! 
 */
+ 
+Или
 
-let smile = ':)';
-let smileList = '';
+// let a = 16
 
-for (let i = 1; i<= 5; i = i + 1) {
-    smileList = smileList + smile;
-}    
-console.log(smileList)
+/* 
+if (a-25 <0)
+alert('Мы в минусе!')
+
+if (a-14 >0)
+alert('Мы в плюсе!')
+*/
