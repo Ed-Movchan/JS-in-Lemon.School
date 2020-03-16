06f6c837f258eca033d12646a7173b14458e724e
Ed_Mvchn <iamtarx@gmail.com>
2020-03-16 18:49:41 +0200

    DZ3

--- main.js
+++ main.js
@@ -1,253 +1,153 @@
-DZ2
+// DZ3
 
-// Задача №0
+// Создайте 4 любые математические функции  с как минимум одним параметром функции.
 
-let arg = prompt("Введите день недели числом");
-switch (arg) {
-  case '1':
-    alert( 'Понедельник' );
-    break;
-
-  case '2':
-    alert( 'Вторник' );
-    break;
-
-  case '3':
-    alert( 'Среда' );
-    break;
-
-    case '4':
-        alert( 'Четверг' );
-        break;
-        
-  case '5':
-    alert( 'Пятница' );
-    break;   
-    
-  case '6':
-    alert( 'Суббота' );
-    break;
-
-    case '7':
-        alert( 'Воскресенье' );
-        break;
-
-  default:
-    alert( 'Нет такого дня недели' );
+function calc(number1, number2) {
+  return number1 * number2
 }
+alert(calc(22,15))
 
 
-// Задача №1
-
-let arg = prompt("Введите код города");
-switch (arg) {
-  case '905':
-      let city1 = 10*4.15
-    alert(city1);
-    break;
-
-  case '194':
-    let city2 = 10*1.98
-    alert(city2);
-    break;
-
-  case '491':
-    let city3 = 10*2.69
-    alert(city3);
-    break;
-
-  case '800':
-    let city4 = 10*5.00
-    alert(city4);
-    break;   
-
-  default:
-    alert( 'Город отсутствует в базе' );
+function calc1(number1, number2, number3) {
+  return (number1 + number2) / number3
 }
+alert(calc1(3,6,1))
 
 
-// Задача №2
-
-let min = prompt("Введите число от 0 до 59");
-      if (min >= 0 && min <= 14) {
-        console.log ("Первая");
-     }
-      if (min >= 15 && min <= 29) {
-        console.log ("Вторая");
-     }
-     if (min >= 30 && min <= 44) {
-        console.log ("Третья");
-     }
-     if (min >= 45 && min <= 59) {
-        console.log ("Четвертая");
-     }
-
-// Задача №3
-
-let num = prompt("Введите число от 1 до 4");
-      if (num == 1) {
-        let result = "Зима"
-        alert (result);
-     }
-     if (num == 2) {
-        let result = "Весна"
-        alert (result);
-     }
-     if (num == 3) {
-        let result = "Лето"
-        alert (result);
-     }
-     if (num == 4) {
-        let result = "Осень"
-        alert (result);
-     }
-
-// Задача №4
+function calc2(number1, number2, number3, number4) {
+  return (number1 - number2) + (number3 + number4)
+}
+alert(calc2(4,4,3,8))
 
 
-let height = prompt("Введите высоту прямоугольника");
-let width = prompt("Введите ширину прямоугольника");
-      if ( s = height*width ) {
-        alert (s);
-     }
+function calc3(number1, number2, number3, number4, number5) {
+  return number1 / number2 + (number3 - number4 * number5)
+}
+alert(calc3(10,5,3,2,2))
+
+// Создать 3 функции без параметров и return
+// первая выводит ваше имя в консоль .лог
+// Вторая выводит в алерте
+// Третья и в консол.лог и в алерте
 
 
-let heightC = prompt("Введите высоту цилиндра");
-let dC = prompt("Введите диаметр основания цилиндра");
-      if ( v = heightC*dC ) {
-        alert (v);
-     }
-
-let sideN = 3;
-let sideM = 4;
-let k = 0;
-     if (k = (sideN ** 2)+(sideM ** 2)) {
-       console.log(k)  
-     }
-
-// Задача №5
-
-let i = 44;
-while (++i <= 67) 
-console.log( i );
+let myName = function() {
+  console.log('Eduard');
+}   
+myName()
 
 
-let i = 44;
-while (++i <= 670) 
-if (i % 10 == 0)
-console.log( i );
+let myName = function() {
+  alert('Eduard');
+}   
+myName()
 
 
-for (let i = 44; i <= 670; i++) {
-    if (i % 10 == 0) {
-      console.log( i );
-    }
-  }
+let myName = function() {
+  alert('Eduard'); console.log('Eduard')
+}   
+myName()
 
+// Создать  функции (всегда с параметром)
+// Функция 
+// 1)Суммы всех нечетных чисел в передаваемом числе
 
-// Задача №6
-
-let num = prompt('Введите целое число: ', '');
+function sumNumber(num) {
 
 let sum = 0;
  
 while (num > 0) {
-      if (num % 10 % 2 == 0) { 
+      if (num % 2 !== 0) { 
           sum += num % 10; 
       }
       num = num / 10 ^ 0; 
 }
-
-alert(sum);
-
-// Задача №7
-
--
-
-
-// Задача №8
-
-let sum = 0;
-        for (let i = 0; i <= 255; i++)
-        {
-            if (i % 2 == 1) sum += i;
-        }
-        console.log(sum);
-
-
-// Задача №9
-
-let sum = '';
-for (let i=1; i<=10; i=i+1) {
-sum = sum +3 +' * '+i+' = ' +3*i + ',';
-}
 console.log(sum);
-
-
-// Задача №10
-
-let num = +prompt('Введите любое целое положительное число');
-let sum = 0;
-for (let i=1; i<=num; i=i+1) {
-sum = sum + i;
 }
-console.log(sum);
+sumNumber(363)
 
 
-// Задача №11
+// 2)Нахождение наибольшего четного числа в передаваемом числе
 
-let exit = false;
-for(let i = 1; i <= 3; i++){
-    if (exit == true)
-        break;
-   let ans=prompt('Рогатый транспорт');
-switch (ans) {
-  case 'Троллейбус':
-      let answer1 = 'Правильно!'
-    console.log(answer1);
-    exit = true;
+// 3)Нажождения последнего нечетного числа в передаваемом числе
+
+
+// 4)Наибольшее из трех переданных чисел
+
+function compareNumber(number1, number2, number3) {
+    if (number1>number2 && number1>number3)
+console.log(number1);
+
+if (number2>number3 && number2>number1)
+console.log(number2);
+
+if (number3>number1 && number3>number2)
+console.log(number3);
+}
+compareNumber(12,4,9)
+
+// 5)Наименьшее из 5 переданных чисел
+
+function compareNumber(number1, number2, number3, number4, number5) {
+  if (number1<number2 && number1<number3 && number1<number4 && number1<number5)
+console.log(number1);
+
+if (number2<number1 && number2<number3 && number2<number4 && number2<number5)
+console.log(number2);
+
+if (number3<number1 && number3<number2 && number3<number4 && number3<number5)
+console.log(number3);
+
+if (number4<number1 && number4<number2 && number4<number3 && number4<number5)
+console.log(number4);
+
+if (number5<number1 && number5<number2 && number5<number3 && number5<number4)
+console.log(number5);
+}
+compareNumber(12,4,9,50,58)
+
+// 6)Функция, которая выводит в консоль тип передаваемого параметра -строка,число и т.д
+
+function showType(parameter) {
+let result = typeof(parameter)
+console.log(result)
+}
+showType("Word")
+
+// 7)Функция, которая получает число дня недели и выводит в алерт Название дня
+
+function showDays(dayNumber) {
+switch (dayNumber) {
+  case 1:
+    alert( 'Monday' );
     break;
 
-  case 'Сдаюсь':
-      let answer2 = 'Правильный ответ: Троллейбус'
-    console.log(answer2);
-    exit = true;
+    case 2:
+    alert( 'Tuesday' );
     break;
 
-  default:
-      let answer3 = 'Подумай еще'
-    console.log(answer3);
+    case 3:
+    alert( 'Wednesday' );
+    break;
+
+    case 4:
+    alert( 'Thursday' );
+    break;
+
+    case 5:
+    alert( 'Friday' );
+    break;
+
+    case 6:
+    alert( 'Saturday' );
+    break;
+
+    case 7:
+    alert( 'Sunday' );
+    break;
+
+    default:
+    alert( 'Go to sleep, progammer' );
 }
 }
-
-
-// Задача №12
-
-let num1 = +prompt('Введите число1');
-let num2 = +prompt('Введите число2');
-let sum = 0;
-
-for (let i=num1; i<=num2; i+=2){
-  sum +=i;
-}
-console.log(sum)
-
-
-// Задача №13
-
-let num = +prompt('Введите любое  число');
-let calc = 1;
-for (let i=1; i<=num; i++) {
-calc = calc * i;
-}
-console.log(calc);
-
-
-// Задача №14
-
-let num = 2;
-let calc = 1;
-for (i=10; i>0; i=i-1) {
-calc = calc * num;
-}
-console.log(calc);
+showDays(7)
