8b0f395df01d7fc921a114e13471d39587e503ef
Ed_Mvchn <iamtarx@gmail.com>
2020-04-15 10:43:25 +0300

    DZ Ajax

--- main.js
+++ main.js
@@ -1,180 +1,55 @@
-// DZ7
 
-// Создайте кнопку, при клике на которую, она будет скрывать сама себя.
+// DZ AJAX
 
-<input type="button" onclick="this.hidden=true" value="Нажми, и я спрячусь"></input>
-
-// Используя JavaScript, сделайте так, чтобы при клике на кнопку исчезал элемент с id=hide;
-
-let elem = document.getElementById('click');
-elem.addEventListener('click', clickRemove);
-
-function clickRemove(){
-    let hide = document.getElementById('hide');
-    hide.remove();
+let module_todos_list = (function () {
+    function ajax(url, callback) {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.onreadystatechange = function () {
+            //if (this.readyState == 4 && this.status == 200)
+            if (this.readyState == 4) {
+                if (this.status == 200)
+                    callback(JSON.parse(this.responseText));
+            }
+        };
+        xhr.send(null);
     }
-
-// Задача. состоит из трёх частей.
-// Сделайте список, элементы которого можно выделять кликом.
-
-let ul = document.getElementsByTagName('ul')[0];
-
-ul.onclick = function(e) {
-  e = e || event;
-  let target = e.target || event.srcElement;
-
-  if (target.tagName != "LI") return;
-
-  selectSingle(target);
-}
-
-ul.onselectstart = ul.onmousedown = function() {
-  return false;
-};
-
-function selectSingle(li) {
-  deselectAll();
-  li.className = 'selected';
-}
-
-function deselectAll() {
-  for(var i=0; i<ul.children.length; i++) {
-    ul.children[i].className = '';
-  }
-}
-
-// Добавьте мульти-выделение. Если клик с нажатым Ctrl (Cmd под Mac), 
-// то элемент добавляется-удаляется из выделенных.
-
-let ul = document.getElementsByTagName('ul')[0];
-
-ul.onclick = function(e) {
-  e = e || event;
-  let target = e.target || event.srcElement;
-
-  if (target.tagName != "LI") return;
-
-  let isMac = navigator.platform.indexOf("Mac") != -1;
-  if(isMac ? e.metaKey : e.ctrlKey) { 
-    toggleSelect(target);
-  } else {
-    selectSingle(target);
-  }
-}
-
-ul.onselectstart = ul.onmousedown = function() {
-  return false;
-};
-
-function toggleSelect(li) {
-  li.className = (li.className == '') ? 'selected' : '';
-}
-
-function deselectAll() {
-  for(i=0; i<ul.children.length; i++) {
-    ul.children[i].className = '';
-  }
-}
-
-function selectSingle(li) {
-  deselectAll();
-  li.className = 'selected';
-}
-
-// Добавьте выделение промежутков. Если происходит клик с нажатым Shift, 
-// то к выделению добавляется промежуток элементов от предыдущего кликнутого до этого. 
-// При этом не важно, какое именно действие делал предыдущий клик.
-
-let ul = document.getElementsByTagName('ul')[0];
-
-let lastClickedLi = null;
-
-ul.onclick = function(e) {
-  e = e || event;
-  let target = e.target || event.srcElement;
-
-  if (target.tagName != "LI") return;
-
-  let isMac = navigator.platform.indexOf("Mac") != -1;
-  if(isMac ? e.metaKey : e.ctrlKey) { 
-    toggleSelect(target);
-  } else if (e.shiftKey) {
-    selectFromLast(target);
-  } else {
-    selectSingle(target);
-  }
-
-  lastClickedLi = target;
-}
-
-ul.onselectstart = ul.onmousedown = function() {
-  return false;
-}
-
-function toggleSelect(li) {
-  li.className = (li.className == '') ? 'selected' : '';
-}
-
-function selectFromLast(target) {
-  let startElem = lastClickedLi || ul.children[0];
-
-  let isLastClickedBefore = compareDocumentPosition(startElem, target) & 4;
-
-  if (isLastClickedBefore) {
-    for(let elem = startElem; elem != target; elem = elem.nextSibling) {
-      elem.className = 'selected';
-    }
-  } else {
-    for(let elem = startElem; elem != target; elem = elem.previousSibling) {
-      elem.className = 'selected';
-    }
-  }
-  elem.className = 'selected';
-
-}
-
-function deselectAll() {
-  for(i=0; i<ul.children.length; i++) {
-    ul.children[i].className = '';
-  }
-}
-
-function selectSingle(li) {
-  deselectAll();
-  li.className = 'selected';
-}
-
-function compareDocumentPosition(a, b) {
-  return a.compareDocumentPosition ?
-    a.compareDocumentPosition(b) :
-      (a != b && a.contains(b) && 16) +
-        (a != b && b.contains(a) && 8) +
-        (a.sourceIndex >= 0 && b.sourceIndex >= 0 ?
-          (a.sourceIndex < b.sourceIndex && 4) +
-            (a.sourceIndex > b.sourceIndex && 2) :
-          1);
-}
-
-
-// Напишите код, который запрещает прокрутку страницы при помощи мыши и клавиш.
-
-document.onwheel = function() {
-    return false;
-  };
   
-  document.addEventListener ("MozMousePixelScroll",
-    function() { 
-        return false }, 
-        false);
-  
-  document.onkeydown = function(e) {
-    if (e.keyCode >= 33 && e.keyCode <= 40) return false;
-  }
+    ajax('https://jsonplaceholder.typicode.com/todos', function (data) {
+        let list = document.getElementById('todos_list');
+        let currentIndex = 0;
+        let currentLimit = 10; 
+        let button = document.getElementById('MoreButton');
+        button.addEventListener('click', showTodos);
 
-// Задача. Переместить элемент в координаты клика мышки.
-// Т.е. пользователь кликает и элемент (div, например) передвигается туда.
+        function showTodos() {
+            currentLimit += currentIndex;
+            for (currentIndex; currentIndex < currentLimit && currentIndex < data.length; currentIndex++) {
+              list.innerHTML += data[currentIndex] + "<br />"; 
+            };
+        data.map(function (el) {   
+            
+          let div_todos = document.createElement('div');
+          div_todos.className = 'todo';
+          let todos = document.createTextNode('todo:');
+          let todo_userId = document.createTextNode('userId:' + el.userId);
+          let todo_id = document.createTextNode('id:' + el.id);
+          let todo_title = document.createTextNode('title:' + el.title);
+          let todo_completed = document.createTextNode('completed:' + el.completed);
+          div_todos.appendChild(todos);		
+          div_todos.appendChild(document.createElement("p"));
+          div_todos.appendChild(todo_userId);		
+          div_todos.appendChild(document.createElement("br"));
+          div_todos.appendChild(todo_id);		
+          div_todos.appendChild(document.createElement("br"));
+          div_todos.appendChild(todo_title);		
+          div_todos.appendChild(document.createElement("br"));
+          div_todos.appendChild(todo_completed);		
+          div_todos.appendChild(document.createElement("br"));
+          div_todos.appendChild(document.createElement("br"));
+          list.appendChild(div_todos);			
+            });	
 
-document.onclick = function(e){
-    mydiv.style.left = e.pageX + "px";
-    mydiv.style.top = e.pageY + "px";
-}
+            }
+    });
+})();
